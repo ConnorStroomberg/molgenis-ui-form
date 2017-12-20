0a7e3c40c5ad789af04537639839b46575839114
connor.stroomberg <connorstroomberg@users.noreply.github.com>
2017-12-20 22:12:56 +0100

    add vue-form + fix lincense + remove index from production build

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,20 +1,54 @@
 <template>
   <div>
-    <form>
-      <div class="form-group">
-        <label for="textInput">Fill in something</label>
-        <input v-model="msg" class="form-control" type="text" id="textInput"/>
+    <vue-form :state="formstate" @submit.prevent="onSubmit">
+
+      <validate auto-label class="form-group required-field" :class="fieldClassName(formstate.name)">
+        <label>Name</label>
+        <input type="text" name="name" class="form-control" required v-model.lazy="model.name">
+
+        <field-messages name="name" show="$touched || $submitted" class="form-control-feedback">
+          <div>Success!</div>
+          <div slot="required">Name is a required field</div>
+        </field-messages>
+
+      </validate>
+
+      <div class="py-2 text-center">
+        <button class="btn btn-primary" type="submit">Submit</button>
       </div>
-    </form>
+
+    </vue-form>
   </div>
 </template>
 
 <script>
+import VueForm from 'vue-form'
+
 export default {
   name: 'FormComponent',
+  mixins: [VueForm],
   data () {
     return {
-      msg: 'Hello'
+      formstate: {},
+      model: {
+        name: 'Hello'
+      }
+    }
+  },
+  methods: {
+    fieldClassName: function (field) {
+      if (!field) {
+        return ''
+      }
+      if ((field.$touched || field.$submitted) && field.$valid) {
+        return 'has-success'
+      }
+      if ((field.$touched || field.$submitted) && field.$invalid) {
+        return 'has-danger'
+      }
+    },
+    onSubmit: function () {
+      console.log(this.formstate.$valid)
     }
   }
 }
