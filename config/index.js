3f2a828ecf6e7a07f3e1ec535143187400292e56
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-09-20 08:52:31 +0200

    Add i18n 'Not unique' message
    - add default message
    - extend mock response with not_unique message

--- config/index.js
+++ config/index.js
@@ -60,6 +60,7 @@ module.exports = {
         const localizedMessages = {
           'form_required_field': 'This field is required',
           'form_validation_failed': 'Validation failed',
+          'form_not_unique': 'Not a unique value',
           'form_not_a_valid_number': 'Not a valid number',
           'form_not_a_valid_integer': 'Not a valid integer value',
           'form_not_a_valid_long': 'Not a valid long value',
